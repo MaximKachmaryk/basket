24a7d18d3a48eea3227188a63eb9d87b7af0834a
maxim <maximkachmaryk12@gmail.com>
2022-06-19 15:24:43 +0300

    part1

---
+++ script.js
@@ -0,0 +1,10 @@
+/* const getSelectValue = document.querySelector("form-select").value;
+console.log(getSelectValue); */
+
+/* function getSelectValue() {
+    const getSelectValue = document.querySelector("form-select").value;
+    console.log(getSelectValue);
+} */
+document.querySelector("form-select").addEventListener('click', () => {
+    document.querySelector("form-select").value
+})
