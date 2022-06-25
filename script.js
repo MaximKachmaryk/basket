f3b46f806f161d4fe1bd0a16eec8a5acba208139
maxim <maximkachmaryk12@gmail.com>
2022-06-25 15:46:52 +0300

    p

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 //отримання даниз з форми select та присвоєння цін 
 
-
-
+const valueColoredit = document.getElementById('valColor').value;
+const priseShoes = 3790;
 const priseRaincoatAction = 1490;
 const priseRaincoat = 2090;
 const PriseColor = document.querySelector(".first_prise");
 const Photo = document.querySelector('.img__raincoat img');
 
-
+const finalPrise = document.querySelector('.total-price');
 
 document.getElementById("val").addEventListener('click', () => {
 
@@ -17,7 +17,7 @@ document.getElementById("val").addEventListener('click', () => {
     const finalPrise = document.querySelector('.total-price');
     finalPrise.innerHTML = valueFirstItem;
     if (valueFirstItem == 1) {
-        finalPrise.innerHTML = priseRaincoatAction;
+        finalPrise.innerHTML = priseRaincoat;
 
     } else if (valueFirstItem == 2) {
         finalPrise.innerHTML = priseRaincoat * 2;
@@ -25,19 +25,28 @@ document.getElementById("val").addEventListener('click', () => {
     } else if (valueFirstItem == 3) {
         finalPrise.innerHTML = priseRaincoat * 3;
     }
+    /* else if (valueFirstItem === 1 || valueColoredit === 2) {
+           finalPrise.innerHTML = priseRaincoatAction;
+
+       } else if (valueFirstItem === 2 || valueColoredit === 2) {
+           finalPrise.innerHTML = priseRaincoatAction * 2;
+
+       } else if (valueFirstItem === 3 || valueColoredit === 2) {
+           finalPrise.innerHTML = priseRaincoatAction * 3;
+       } */
 })
 
 document.getElementById("valColor").addEventListener('click', () => {
     const valueColor = document.getElementById('valColor').value;
     console.log(valueColor);
     if (valueColor == 1) {
-        PriseColor.innerHTML = 2490;
+        PriseColor.innerHTML = priseRaincoat;
         Photo.src = '/img/png.jpg';
     }
 
 
     if (valueColor == 2) {
-        PriseColor.innerHTML = 1490;
+        PriseColor.innerHTML = priseRaincoatAction;
         Photo.src = '/img/blue.jpg';
 
 
@@ -61,13 +70,14 @@ document.getElementById("shoes").addEventListener('click', () => {
     let finalPriseShoes = document.querySelector('.total-price');
     finalPriseShoes.innerHTML = valueFirstItemShoes;
     if (valueFirstItemShoes == 1) {
-        finalPriseShoes.innerHTML = 1490;
+        finalPriseShoes.innerHTML = priseShoes;
 
     } else if (valueFirstItemShoes == 2) {
-        finalPriseShoes.innerHTML = 2090 * 2;
+        finalPriseShoes.innerHTML = priseShoes * 2;
 
     } else if (valueFirstItemShoes == 3) {
-        finalPriseShoes.innerHTML = 2090 * 3;
+        finalPriseShoes.innerHTML = priseShoes * 3;
 
     }
 })
+console.log("color", valueColoredit);
